10991faa9959a7c035e888fcc19acd268cb8e7b9
torkjels <tskjetne@gmail.com>
2017-04-28 14:48:02 +0200

    fetch post with body

--- app/utils/fetchBuilder.js
+++ app/utils/fetchBuilder.js
@@ -1,6 +1,7 @@
-export const fetchGet = (url) => fetch(url, {credentials: 'include', method: 'GET'})
-export const fetchPut = (url) => fetch(url, {credentials: 'include', method: 'PUT'})
+export const fetchGet = (url) => fetch(url, {credentials: 'include', method: 'GET'}).then((res) => res.json())
+export const fetchPut = (url) => fetch(url, {credentials: 'include', method: 'PUT'}).then((res) => res.json())
+export const fetchPost = (url, body) => fetch(url, {credentials: 'include', method: 'POST', body: JSON.stringify(body)})
 
 export default {
-	fetchGet, fetchPut
+	fetchGet, fetchPut, fetchPost
 }
