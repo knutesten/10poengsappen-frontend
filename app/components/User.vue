dcea16658cd7f2add7155a8a6ca803e5e30ffddd
torkjels <tskjetne@gmail.com>
2017-05-02 10:41:26 +0200

    move points margin to absolute element to exclude it from centering

--- app/components/User.vue
+++ app/components/User.vue
@@ -18,25 +18,25 @@
 </template>
 
 <script>
-    export default {
-        props: ['user'],
-        methods: {
-            showPointsInput(userId) {
-                this.$store.commit('showPointsInput', userId)
-            },
-            rewardPoints(user, event) {
-                this.$store.dispatch('rewardPoints', {user, points: parseInt(this.$refs.pointsInput.value || 0)})
-            }
-        },
-        computed: {
-            loggedInUser() {
-                return this.$store.state.user
-            },
-            showInput() {
-                return this.$store.state.showPointsInput
-            }
-        }
-    }
+	export default {
+		props: ['user'],
+		methods: {
+			showPointsInput(userId) {
+				this.$store.commit('showPointsInput', userId)
+			},
+			rewardPoints(user, event) {
+				this.$store.dispatch('rewardPoints', {user, points: parseInt(this.$refs.pointsInput.value || 0)})
+			}
+		},
+		computed: {
+			loggedInUser() {
+				return this.$store.state.user
+			},
+			showInput() {
+				return this.$store.state.showPointsInput
+			}
+		}
+	}
 </script>
 
 <style lang="scss" scoped>
@@ -46,73 +46,71 @@
         border-bottom: 0.12rem solid whitesmoke;
         padding: 0.5rem;
 
-    .name, .points {
-        width: 50%;
-        text-align: center;
-    }
-
-    .action-buttons {
-        display: flex;
-        align-items: center;
-
-        button {
-            opacity: 0.3;
-
-            &:hover {
-                opacity: 1;
-             }
+        .name, .points {
+            width: 50%;
+            text-align: center;
         }
 
-        .btn-reward-points {
-            border: 0;
-            font-size: 1.1rem;
+        .action-buttons {
+            display: flex;
+            margin-left: 0.4rem;
+            align-items: center;
+
+            button {
+                opacity: 0.3;
+
+                &:hover {
+                    opacity: 1;
+                }
+            }
+
+            .btn-reward-points {
+                border: 0;
+                font-size: 1.1rem;
+            }
+
+            .btn-undo {
+                margin-left: 0.2rem;
+                padding: 0.2rem 0.3rem;
+            }
         }
 
-        .btn-undo {
-            margin-left: 0.2rem;
-            padding: 0.2rem 0.3rem;
+        .points {
+            display: flex;
+            justify-content: center;
+            align-items: center;
         }
-    }
 
-    .points {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+        .reward-points-container {
+            width: 0;
+            position: relative;
+            cursor: pointer;
 
-    .reward-points-container {
-        width: 0;
-        margin-left: 0.4rem;
-        position: relative;
-        cursor: pointer;
+            .btn-give-points {
+                opacity: 0.6;
 
-    .btn-give-points {
-        opacity: 0.6;
+                & :hover {
+                    opacity: 1;
+                }
 
-    &
-    :hover {
-        opacity: 1;
-    }
+                & :focus {
+                    outline: none;
+                }
 
-    &
-    :focus {
-        outline: none;
-    }
+            }
 
-    }
+            .give-points-input-container {
+                left: -100px;
+                display: flex;
 
-    .give-points-input-container {
-        left: -100px;
-        display: flex;
+                input {
+                    font-size: 1rem;
+                    height: 1.2rem;
+                    width: 2.5rem;
+                    margin-right: 0.3rem;
+                }
 
-    input {
-        font-size: 1rem;
-        height: 1.2rem;
-        width: 2.5rem;
-        margin-right: 0.3rem;
-    }
-
-    }
-    }
+            }
+        }
     }
 </style>
